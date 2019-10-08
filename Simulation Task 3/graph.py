9860f8f93d5f1881384437063b731489f24299dd
Justin Hu <justi801514@gmail.om>
2019-10-08 00:21:21 -0400

    finalize

--- Simulation Task 3/graph.py
+++ Simulation Task 3/graph.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-mean_df = pd.read_csv('mean_outputs.csv',names=["RT_mean", "RT_mean_lower_CI", "RT_mean_upper_CI", "nonRT_mean", "nonRT_mean_lower_CI", "RT_mean_upper_CI"])
-percentile_df = pd.read_csv('95thPercentile_outputs.csv',names=["RT_95th", "RT_95th_lower_CI", "RT_95th_upper_CI", "nonRT_95th", "nonRT_95th_lower_CI", "RT_95th_upper_CI"])
+mean_df = pd.read_csv('./data_output/mean_outputs.csv',names=["RT_mean", "RT_mean_lower_CI", "RT_mean_upper_CI", "nonRT_mean", "nonRT_mean_lower_CI", "RT_mean_upper_CI"])
+percentile_df = pd.read_csv('./data_output/95thPercentile_outputs.csv',names=["RT_95th", "RT_95th_lower_CI", "RT_95th_upper_CI", "nonRT_95th", "nonRT_95th_lower_CI", "RT_95th_upper_CI"])
 
 # mean_df.to_csv("mean_outputs.csv")
 # percentile_df.to_csv("95thPercentile_outputs.csv")
@@ -25,15 +25,19 @@ r2 = [x + barWidth for x in r1]
  
 # Create blue bars
 plt.bar(r1, RT_bar, width = barWidth, color = 'blue', edgecolor = 'black', yerr=RT_yerr, capsize=7, label='RT mean')
-plt.bar(r2, nonRT_bar, width = barWidth, color = 'red', edgecolor = 'black', yerr=nonRT_yerr, capsize=7, label='nonRT mean')
+plt.bar(r2, nonRT_bar, width = barWidth, color = 'brown', edgecolor = 'black', yerr=nonRT_yerr, capsize=7, label='nonRT mean')
  
 # Plot mean graph
 plt.xticks([r + barWidth for r in range(len(RT_bar))], ['10', '15', '20', '25', '30', '35', '40'])
 plt.ylabel('Mean')
 plt.xlabel('MIAT of nonRT')
+plt.plot(RT_bar)
+plt.plot(nonRT_bar)
 plt.legend()
+plt.title("Mean Plot")
 plt.savefig('mean_plot.png')
 plt.close()
+
 
 # Input means as height of bar
 RT_bar = percentile_df["RT_95th"].tolist()
@@ -49,12 +53,15 @@ r2 = [x + barWidth for x in r1]
  
 # Create blue bars
 plt.bar(r1, RT_bar, width = barWidth, color = 'blue', edgecolor = 'black', yerr=RT_yerr, capsize=7, label='RT 95th percentile')
-plt.bar(r2, nonRT_bar, width = barWidth, color = 'red', edgecolor = 'black', yerr=nonRT_yerr, capsize=7, label='nonRT 95th percentile')
+plt.bar(r2, nonRT_bar, width = barWidth, color = 'brown', edgecolor = 'black', yerr=nonRT_yerr, capsize=7, label='nonRT 95th percentile')
  
 # Plot 95th percentile graph
 plt.xticks([r + barWidth for r in range(len(RT_bar))], ['10', '15', '20', '25', '30', '35', '40'])
 plt.ylabel('95th Percentile')
 plt.xlabel('MIAT of nonRT')
+plt.plot(RT_bar)
+plt.plot(nonRT_bar)
 plt.legend()
+plt.title("95th Percentile Plot")
 plt.savefig('95percentile_plot.png')
 plt.close()
--- Simulation Task 3/readme.md
+++ Simulation Task 3/readme.md
@@ -11,15 +11,22 @@ scp whu24_Task3.zip <Unity_id>@remote-linux.eos.ncsu.edu:/afs/u​nity.ncsu.edu/
 ```
 To run the script unzip the zipfile and use python compiler
 ```
-unzip whu24_Task3.zip
+unzip whu24_task3.zip
 python ./task3/simulation_task3.py
 ```
 Or
 ```
 unzip task3
 cd task3
-python simulation_task3.py
+python ./task3/simulation_task3.py
 ```
-I used `matplotlib` package for ploting the results as required, but it is not originally installed on the eos server. To make the `graph.py` compiled correctly, it requires to run on VCL Ubuntu 18.04 and intall the `pip` and run `pip install -r requirement.txt`.
-Please note that the default seed value is 100, update the seed value manaually to match the test condition. Thanks!
-The results will display on terminal as well as store into `mean_outputs.csv` and `95thPercentile_outputs.csv` for ploting the graph. 
+
+I used the packages which are not included in the eos original environment, if TA would like to run `graph.py` please select `CSC591_ADBI_v3` in the VCL reservation portal, which is another course I took but already had most of package installed, and run the instructions as below:
+```
+python3 graph.py
+```
+After that, `mean_plot.png` and `95thpercentile.png` in the folder. As for the data for plotting the graph, I used the data with various MIAT nonRT from requirement and store into `*.csv`.
+
+p.s. The zip file included the original data for plotting, note that the csv will be added more data everytime when the `simulation_task3.py` run. 
+
+p.p.s. Actually my code should be compatible with python > 2.7 and python3 > 3.5, so it should not have problem with running code on `CSC591_ADBI_v3` VCL. However, I think that python3 has better compatibility with `matplotlib`, which is the package I used for plotting, so that I choose run `graph.py` with `python3` instead of `python`. I tried many ways to setup `matplotlib` in python 2.7 environment but it requires admin permission. I would suggest that instructors can setup an environment in VCL and allow student to reserve and test it to ensure both testing results are consistent. 
